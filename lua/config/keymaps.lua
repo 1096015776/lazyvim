de9b6b7c272de581c45be1246bf06dc754111819
fx <fx@qq.com>
2023-05-31 23:09:14 +0800

    refactor:move map function to util

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,19 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- local Util = require("lazyvim.util")
-
-local function map(mode, lhs, rhs, opts)
-  local keys = require("lazy.core.handler").handlers.keys
-  ---@cast keys LazyKeysHandler
-  -- do not create the keymap if a lazy keys handler exists
-  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
-    opts = opts or {}
-    opts.silent = opts.silent ~= false
-    vim.keymap.set(mode, lhs, rhs, opts)
-  end
-end
+local map = require("util.index").map
 
 -- easy esc
 map("i", "jk", "<esc>", { desc = "esc" })
@@ -42,5 +27,7 @@ map("n", "gk", ':lua require"gitsigns".prev_hunk()<cr>')
 map("n", "gj", ':lua require"gitsigns".next_hunk()<cr>')
 map("n", "<c-q>", ":q<cr>")
 map("n", "<leader><leader>", ":e #<cr>")
+
+-- tab
 map("n", "<c-g>", ":tabe<cr>:-tabmove<cr>:term lazygit<cr>i")
 map("n", "<c-e>", ":tabe<cr>:-tabmove<cr>:term ranger<cr>i")

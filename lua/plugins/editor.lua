8f8e54686e2c8c7a837ba8a0ea549bd7e4d8f87c
fx <fx@qq.com>
2023-05-31 23:37:56 +0800

    fix:change surround map

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -65,4 +65,18 @@ return {
       position = "right",
     },
   },
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "S",
+        delete = "ds",
+        -- find = "",
+        -- find_left = "gsF",
+        -- highlight = "gsh",
+        replace = "cs",
+        -- update_n_lines = "gsn",
+      },
+    },
+  },
 }

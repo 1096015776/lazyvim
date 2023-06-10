31dfb8ea70443823c877802ebd08d2c4bfa7251f
fx <fx@qq.com>
2023-06-10 18:37:53 +0800

    enable format

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,16 +2,9 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- vim.api.nvim_create_autocmd("TermClose", {
+-- vim.api.nvim_create_autocmd("FileType", {
 --   pattern = "*",
 --   callback = function()
---     vim.cmd("bd")
+--     vim.b.autoformat = false
 --   end,
 -- })
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "*",
-  callback = function()
-    vim.b.autoformat = false
-  end,
-})

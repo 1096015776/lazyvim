4887bea1352922ffa0a9b71accde633612ce8b0c
fx <fx@qq.com>
2023-06-11 21:41:47 +0800

    change tmux config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -32,3 +32,12 @@ map("n", "<leader>1", function()
     previewer = false,
   })
 end)
+
+-- tmux
+local nvim_tmux_nav = require("nvim-tmux-navigation")
+map("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+map("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+map("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+map("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+map("n", "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+map("n", "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
